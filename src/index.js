5355c258607db3848e54fc938038faa6f1df3ddb
Danish <khandanish36408@gmail.com>
2024-03-23 16:31:29 +0530

    :fire:update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'; 
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './redux/rootReducer';
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+// import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const firebaseConfig = {
+  apiKey: "AIzaSyBFOnRtnBthsrqWQRzuee5QMGQUv7uReLg",
+  authDomain: "resume-builder-399c2.firebaseapp.com",
+  projectId: "resume-builder-399c2",
+  storageBucket: "resume-builder-399c2.appspot.com",
+  messagingSenderId: "152029610315",
+  appId: "1:152029610315:web:2b0e9a6597e6ed4c4ca8ce"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore()
+
+const reduxStore = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk.withExtraArgument({ getFirebase , getFirestore })),reduxFirestore(firebase))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={reduxStore}>
+      <ReactReduxFirebaseProvider
+        firebase={firebase}
+        config={firebaseConfig}
+        dispatch={reduxStore.dispatch}
+        createFirestoreInstance={createFirestoreInstance}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
